d4766415f9c8282c14cdb77791096e488a243576
clnash <nash_web@outlook.com>
2016-06-27 23:56:02 +0000

    Added bootstrap cdn, as well as functionality to create a form submition through /campground/new to add new campground to array(soon to be database).

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var app = express();
 
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
 
+//add some campgrounds to campground page
 var campgrounds = [
     {name: "Kachess Lake", image:"http://www.kachessridgeresort.com/uploads/1/0/2/5/10252907/1325803461.jpg"},
     {name: "Lake Chelan", image:"http://www.nwseaplanes.com/wp-content/uploads/lake-chelan.jpg"},
@@ -12,27 +15,36 @@ var campgrounds = [
     {name: "Mount Rainier National Park", image:"https://upload.wikimedia.org/wikipedia/commons/3/32/Mount_Rainier_from_above_Myrtle_Falls_in_August.JPG"},
     {name: "Olympic National Park", image:"http://images.boomsbeat.com/data/images/full/23858/37-jpg.jpg"}
                ];
-
+//set view engine
 app.set("view engine", 'ejs');
+
 //Set the root route
 app.get('/', function(req, res){
-    
-    
     res.render("landing");
-    
 })
 
 //Campgrounds page
 app.get('/campgrounds', function(req, res){
-    
-    res.render("campgrounds", {campgrounds:campgrounds});
+       res.render("campgrounds", {campgrounds:campgrounds});
+})
+// add new campgrounds
+app.post('/campgrounds', function(req, res){
+    //get data from form and add to campgrounds array
+    var name = req.body.name;
+    var image = req.body.image;
+    //create new object to push into array
+    var newCampground = {name: name, image: image};
+    campgrounds.push(newCampground);
+    //redirect back to /campgrounds
+    res.redirect('/campgrounds');
+})
+
+app.get('/campgrounds/new', function(req, res){
+    res.render('new.ejs');
 })
 
 
-
+//listen for server
 app.listen(process.env.PORT, process.env.IP, function(){
-    
-    
     console.log('Take a hike!');
-    
 })
